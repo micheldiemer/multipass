cbe1db9589839c65c69f712b68c806a2a680a90f
Michel Diemer <michel.diemer@yahoo.fr>
2024-04-24 16:06:42 +0200

    ajout test existence fichier

--- newvm/09-samba.bat
+++ newvm/09-samba.bat
@@ -1,4 +1,9 @@
-multipass transfer "%~dp0.smbcredentials" dev:/home/ubuntu/.smbcredentials
+IF NOT EXIST "%~dp0.smbcredentials" (
+   echo Fichier .smbcredentials manquant
+   exit
+)
+
+ultipass transfer "%~dp0.smbcredentials" dev:/home/ubuntu/.smbcredentials
 multipass transfer "%~dp0smb.ini" dev:/home/ubuntu/smb.conf
 
 multipass transfer "%~dp009-samba.sh" dev:/tmp/x.sh
